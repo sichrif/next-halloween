90dfced1f6cf5baa9b9086e85bb6fc619d210728
sichrif <mchrif000@yandex.com>
2022-03-16 16:23:27 +0100

    fixing builds

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  "presets": ["next/babel"],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
